54c1aeeb4379454c0fc643ea83c9c61fb51a3828
Mert5558 <149884808+Mert5558@users.noreply.github.com>
2024-02-20 14:04:39 +0100

    Add files via upload

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: merdal <merdal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/19 14:43:04 by merdal            #+#    #+#             */
-/*   Updated: 2024/02/19 14:43:09 by merdal           ###   ########.fr       */
+/*   Updated: 2024/02/20 12:49:50 by merdal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,7 +66,6 @@ void	ft_putnbr_fd(int n, int fd);
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
-t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstdelone(t_list *lst, void (*del)(void*));
 void	ft_lstclear(t_list **lst, void (*del)(void*));
