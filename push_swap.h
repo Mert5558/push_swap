a2b925618eac744ad5f2af2fe0afef9ccb41d772
Mert5558 <149884808+Mert5558@users.noreply.github.com>
2024-02-26 14:23:33 +0100

    Add files via upload

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: merdal <merdal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/19 11:49:06 by merdal            #+#    #+#             */
-/*   Updated: 2024/02/20 13:52:41 by merdal           ###   ########.fr       */
+/*   Updated: 2024/02/26 12:44:49 by merdal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,18 @@ void	ft_add_nodes(t_stack **stack, int content);
 t_stack	*ft_process_normal(int argc, char **argv);
 t_stack	*ft_process_quotation(char **argv);
 t_stack	*ft_lstlast(t_stack *lst);
+int		ft_lstsize(t_stack *lst);
 int		ft_checkfordup(t_stack *a);
 void	ft_free(t_stack **lst);
+int		ft_checkifsorted(t_stack *stack_a);
+void	ft_sa(t_stack **a, int i);
+void	ft_sb(t_stack **b, int i);
+void 	ft_pa(t_stack **a, t_stack **b, int i);
+void	ft_pb(t_stack **b, t_stack **a, int i);
+void	ft_ra(t_stack **a, int i);
+void	ft_rb(t_stack **b, int i);
+void	ft_rra(t_stack **a, int i);
+void	ft_rrb(t_stack **b, int i);
+void	ft_sort(t_stack **a);
 
 #endif
