16aecd11770a2305af8a6394e2880a45c2c6ee83
Mert5558 <149884808+Mert5558@users.noreply.github.com>
2024-03-01 10:51:54 +0100

    Add files via upload

--- sorting.c
+++ sorting.c
@@ -6,7 +6,7 @@
 /*   By: merdal <merdal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/26 12:27:59 by merdal            #+#    #+#             */
-/*   Updated: 2024/02/29 14:55:49 by merdal           ###   ########.fr       */
+/*   Updated: 2024/03/01 10:49:27 by merdal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -104,12 +104,46 @@ void	ft_compare_moves(t_stack **a)
 	}
 }
 
+int	ft_num_top(t_stack **stack)
+{
+	int	num_top;
+
+	num_top = (*stack)->num;
+	return (num_top);
+}
+
+void	ft_smallest_to_top(t_stack **b)
+{
+	while (ft_find_smallest(b) != 0)
+	{
+		ft_rb(b, 0);
+	}
+}
+
+void	ft_check_push(t_stack **a, t_stack **b)
+{
+	int	top_a;
+	int	top_b;
+
+	top_a = ft_num_top(a);
+	if (b)
+		top_b = ft_num_top(b);
+	else
+		top_b = - 1;
+	if (top_a - top_b > 0)
+		ft_pb(a, b, 0);
+	else
+	{
+		ft_smallest_to_top(b);
+		ft_pb(a, b, 0);
+	}
+}
+
 void	ft_sort(t_stack **a)
 {
 	t_stack	*b;
-	int	i;
 
 	b = NULL;
-	i = 0;
 	ft_compare_moves(a);
+	ft_check_push(a, &b);
 }
