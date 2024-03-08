1ee43a27413c0cb6f74da507141d3b70786923f0
Mert5558 <149884808+Mert5558@users.noreply.github.com>
2024-03-08 17:33:24 +0100

    Add files via upload

--- scan.c
+++ scan.c
@@ -6,11 +6,20 @@
 /*   By: merdal <merdal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 12:10:36 by merdal            #+#    #+#             */
-/*   Updated: 2024/03/08 15:43:22 by merdal           ###   ########.fr       */
+/*   Updated: 2024/03/08 16:38:19 by merdal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+t_stack	*ft_get_last_stack(t_stack **a)
+{
+	t_stack	*current = *a;
+
+	while (current->next != NULL)
+		current = current->next;
+	return (current);
+}
 
 int	ft_scan_top(t_stack **a, int *chunk)
 {
@@ -20,18 +29,42 @@ int	ft_scan_top(t_stack **a, int *chunk)
 
 	while (current != NULL)
 	{
-		if (current->num == chunk[index_chunk])
+		while (index_chunk <= 20)
 		{
-			printf("%d\n", index_stack);
-			return (index_stack);
+			if (current->num == chunk[index_chunk])
+			{
+				printf("%d\n", index_stack);
+				return (index_stack);
+			}
+			index_chunk++;
 		}
+		index_chunk = 0;
 		current = current->next;
 		index_stack++;
 	}
 	return (0);
 }
 
-void	ft_scan_bottom(t_stack **a, int *chunk)
+int	ft_scan_bottom(t_stack **a, int *chunk)
 {
-	
+	int index_chunk = 0;
+	int	index_stack = ft_lstsize(*a) - 1;
+	t_stack *current = ft_get_last_stack(a);
+
+	while (current != *a)
+	{
+		while (index_chunk <= 20)
+		{
+			if (current->num == chunk[index_chunk])
+			{
+				printf("%d\n", index_stack);
+				return(index_stack);
+			}
+			index_chunk++;
+		}
+		index_chunk = 0;
+		current = current->prev;
+		index_stack--;
+	}
+	return (0);
 }
