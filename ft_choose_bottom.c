c5177b8ef49f6fcc8a860f4b946b676c7ca21302
Mert5558 <149884808+Mert5558@users.noreply.github.com>
2024-03-26 16:03:10 +0100

    Add files via upload

--- ft_choose_bottom.c
+++ ft_choose_bottom.c
@@ -6,7 +6,7 @@
 /*   By: merdal <merdal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/22 16:16:14 by merdal            #+#    #+#             */
-/*   Updated: 2024/03/25 15:17:11 by merdal           ###   ########.fr       */
+/*   Updated: 2024/03/26 12:00:14 by merdal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,27 @@ int	*ft_get_options_bottom(t_stack **a, t_stack **b, int size_options)
 	return (options);
 }
 
-void	ft_choose_bottom(t_stack **a, t_stack **b)
+int	ft_is_in_chunk_2(t_stack **b, int *chunk_2, int size_options)
+{
+	int	i = 0;
+	int	count = 0;
+	t_stack	*current = ft_lstlast(*b);
+	
+	while (count < size_options)
+	{
+		while (chunk_2[i] != '\0')
+		{
+			if (current->num == chunk_2[i])
+				count++;
+			i++;
+		}
+		current = current->prev;
+		i = 0;
+	}
+	return (count);
+}
+
+void	ft_choose_bottom(t_stack **a, t_stack **b, int *chunk_2)
 {
 	int	*options;
 	int	less_moves;
@@ -42,18 +62,17 @@ void	ft_choose_bottom(t_stack **a, t_stack **b)
 	if (ft_lstsize(*b) < size_options)
 		size_options = ft_lstsize(*b);
 	
+	size_options = ft_is_in_chunk_2(b, chunk_2, size_options);
+
 	options = ft_get_options_bottom(a, b, size_options);
-	//print_array(options, size_options);
 	
 	if (!options)
 		ft_error();
 	
 	temp = ft_find_less_moves(options, size_options);
 	less_moves = ft_lstsize(*b) - temp - 1;
-	//printf("less_moves: %d\n", less_moves);
 
 	ft_index_to_top(b, less_moves);
 
 	free(options);
-	// 46 63 2 13 40 64 35 80 49 36 95 3 60 34 17 68 70 73 52 11 38 61 76 59 32 42 54 27 90 99 44 67 78 45 25 23 62 100 12 81 89 53 87 98 15 18 43 75 33 29 82 91 31 92 66 5 71 69 56 47 72 16 21 41 74 94 26 7 86 85 57 84 37 65 58 8 93 4 83 96 77 50 55 14 6 19 88 79 10 39 28 24 9 48 30 22 51 97 20 1
 }
