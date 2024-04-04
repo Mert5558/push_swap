82a70d109581d36a21ef8277d7b93a11bdf678b1
Mert5558 <149884808+Mert5558@users.noreply.github.com>
2024-04-04 16:49:09 +0200

    Add files via upload

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: merdal <merdal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 13:25:39 by merdal            #+#    #+#             */
-/*   Updated: 2023/11/09 11:07:03 by merdal           ###   ########.fr       */
+/*   Updated: 2024/04/04 16:47:41 by merdal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,8 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t n)
 {
 	size_t	i;
 	size_t	x;
-	size_t	len;
 
 	i = 0;
-	len = ft_strlen(needle);
 	if (needle == NULL || needle[i] == '\0')
 		return ((char *)(haystack));
 	while (i < n && haystack[i] != '\0')
