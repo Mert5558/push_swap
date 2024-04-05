1fed39702371592c002489776ffac4ac0f438820
Mert5558 <149884808+Mert5558@users.noreply.github.com>
2024-04-05 16:57:09 +0200

    Add files via upload

--- process_input.c
+++ process_input.c
@@ -6,11 +6,40 @@
 /*   By: merdal <merdal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/19 12:12:12 by merdal            #+#    #+#             */
-/*   Updated: 2024/02/27 16:33:18 by merdal           ###   ########.fr       */
+/*   Updated: 2024/04/05 12:54:34 by merdal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+int	ft_atoi_check(const char *str)
+{
+	int				sign;
+	long long int	i;
+
+	i = 0;
+	sign = 1;
+	while (*str == ' ' || *str == '\t' || *str == '\n' || *str == '\f'
+		|| *str == '\v' || *str == '\r')
+		str++;
+	if (*str == '-' && ft_isdigit(*(str + 1)))
+	{
+		sign = -1;
+		str++;
+	}
+	else if (*str == '+' && ft_isdigit(*(str + 1)))
+		str++;
+	while (*str)
+	{
+		if (!ft_isdigit(*str))
+			ft_error();
+		i = i * 10 + (*str - 48);
+		str++;
+	}
+	if ((sign * i) > 2147483647 || (sign * i) < -2147483648)
+		ft_error();
+	return (sign * i);
+}
 
 t_stack	*ft_process_quotation(char **argv)
 {
@@ -24,10 +53,11 @@ t_stack	*ft_process_quotation(char **argv)
 	temp = ft_split(argv[1], 32);
 	while (temp[i])
 	{
-		j = ft_atoi(temp[i]);
+		j = ft_atoi_check(temp[i]);
 		ft_add_nodes(&a, j);
 		i++;
 	}
+	free(temp);
 	return (a);
 }
 
@@ -41,7 +71,7 @@ t_stack	*ft_process_normal(int argc, char **argv)
 	a = NULL;
 	while (i < argc)
 	{
-		j = ft_atoi(argv[i]);
+		j = ft_atoi_check(argv[i]);
 		ft_add_nodes(&a, j);
 		i++;
 	}
