561384de2fd46edffde7b61ad2ac1ec653c1d9f1
Mert5558 <149884808+Mert5558@users.noreply.github.com>
2024-04-05 16:56:57 +0200

    Add files via upload

--- add_nodes.c
+++ add_nodes.c
@@ -6,7 +6,7 @@
 /*   By: merdal <merdal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/19 12:51:59 by merdal            #+#    #+#             */
-/*   Updated: 2024/03/18 11:04:33 by merdal           ###   ########.fr       */
+/*   Updated: 2024/04/05 15:48:09 by merdal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,31 +24,34 @@ t_stack	*ft_new_node(int content)
 	return (new);
 }
 
-void ft_add_to_stack(t_stack **a, t_stack *new_node)
+void	ft_add_to_stack(t_stack **a, t_stack *new_node)
 {
-    if (!a)
-        return;
-    
-    if (!*a) {
-        *a = new_node;
-        new_node->prev = NULL;
-    } else {
-        t_stack *last_node = ft_lstlast(*a);
-        last_node->next = new_node;
-        new_node->prev = last_node;
-    }
-}
+	t_stack	*last_node;
 
+	if (!a)
+		return ;
+	if (!*a)
+	{
+		*a = new_node;
+		new_node->prev = NULL;
+	}
+	else
+	{
+		last_node = ft_lstlast(*a);
+		last_node->next = new_node;
+		new_node->prev = last_node;
+	}
+}
 
 void	ft_add_nodes(t_stack **a, int content)
 {
-    t_stack	*new_node;
+	t_stack	*new_node;
 
-    new_node = ft_new_node(content);
-    if (!new_node)
-    {
-        ft_error();
-        return ;
-    }
-    ft_add_to_stack(a, new_node);
+	new_node = ft_new_node(content);
+	if (!new_node)
+	{
+		ft_error();
+		return ;
+	}
+	ft_add_to_stack(a, new_node);
 }
